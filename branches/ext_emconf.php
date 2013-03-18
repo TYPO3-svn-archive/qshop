902566a1930f8c50407e5eeee7737043028a970c
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-18 00:49:27 +0000

    3.0.0 +++ Improvements
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/qshop@73240 735d13b6-9817-0410-8766-e36946ffe9aa

--- branches/ext_emconf.php
+++ branches/ext_emconf.php
@@ -42,9 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'cap_tcpdf' => '',
 			'quickshop_installer' => '',
-			'wt_cart_orderpdf' => '',
 		),
 	),
 	'_md5_values_when_last_written' => 'a:30:{s:9:"ChangeLog";s:4:"656e";s:21:"ext_conf_template.txt";s:4:"d56d";s:12:"ext_icon.gif";s:4:"2501";s:14:"ext_tables.php";s:4:"7e8b";s:14:"ext_tables.sql";s:4:"4900";s:16:"locallang_db.xml";s:4:"489e";s:7:"tca.php";s:4:"bbfd";s:14:"doc/manual.pdf";s:4:"bc08";s:14:"doc/manual.sxw";s:4:"bbf8";s:16:"doc/tutorial.pdf";s:4:"a4b5";s:16:"doc/tutorial.sxw";s:4:"cb5b";s:37:"lib/class.tx_quickshop_extmanager.php";s:4:"7e00";s:36:"lib/class.tx_quickshop_userfuncs.php";s:4:"4878";s:17:"lib/locallang.xml";s:4:"750f";s:37:"lib/icons/die-netzmacher.de_200px.gif";s:4:"48b3";s:31:"lib/icons/your-logo_de-blue.gif";s:4:"19f7";s:31:"lib/icons/your-logo_de-grey.gif";s:4:"1fbc";s:36:"lib/icons/your-logo_default-blue.gif";s:4:"710c";s:36:"lib/icons/your-logo_default-grey.gif";s:4:"6fdc";s:15:"res/default.css";s:4:"3093";s:16:"res/default.tmpl";s:4:"6d82";s:23:"res/tmpl_fieldwrap.html";s:4:"c439";s:38:"res/images/alternate_image_400x300.gif";s:4:"91b6";s:44:"res/images/quick_shop_header_image_210px.jpg";s:4:"6cd7";s:20:"res/v1.4/default.css";s:4:"b18a";s:21:"res/v1.4/default.tmpl";s:4:"5a4a";s:20:"static/constants.txt";s:4:"0ddf";s:16:"static/setup.txt";s:4:"aecc";s:31:"static/quick_shop/constants.txt";s:4:"d41d";s:27:"static/quick_shop/setup.txt";s:4:"8e73";}',
