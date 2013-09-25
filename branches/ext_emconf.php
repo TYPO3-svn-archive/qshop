91b321968483dd4a7fad0e1bcd934964514cd80f
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-09-25 22:29:53 +0000

    Release 3.0.8
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/qshop/trunk@80029 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ branches/ext_emconf.php
@@ -0,0 +1,54 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "quick_shop".
+ *
+ * Auto generated 30-03-2013 00:18
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Quick Shop',
+	'description' => 'Quick Shop - the fastest shop in the history of TYPO3. Install the complete shop with one mouse click! Quick Shop itself hasn\'t any line PHP code. You need only TypoScript for extending the shop with new database fields and tables. Quick Shop is a team play with the Browser - TYPO3 without PHP -, Caddy - your shopping cart - and Powermail.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '3.0.8',
+	'dependencies' => 'browser,caddy,cps_tcatree,css_styled_content,powermail',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Dirk Wildt (Die Netzmacher)',
+	'author_email' => 'http://wildt.at.die-netzmacher.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'browser' => '',
+			'caddy' => '',
+			'cps_tcatree' => '',
+			'css_styled_content' => '',
+			'powermail' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:32:{s:9:"ChangeLog";s:4:"bb0a";s:21:"ext_conf_template.txt";s:4:"a049";s:12:"ext_icon.gif";s:4:"2501";s:14:"ext_tables.php";s:4:"0a56";s:14:"ext_tables.sql";s:4:"3389";s:16:"locallang_db.xml";s:4:"c569";s:7:"tca.php";s:4:"d548";s:14:"doc/manual.pdf";s:4:"f5e6";s:14:"doc/manual.sxw";s:4:"cdde";s:16:"doc/tutorial.pdf";s:4:"a4b5";s:16:"doc/tutorial.sxw";s:4:"cb5b";s:37:"lib/class.tx_quickshop_extmanager.php";s:4:"60d5";s:18:"lib/localllang.xml";s:4:"e882";s:41:"lib/icons/die-netzmacher-logo-quadrat.png";s:4:"a7d4";s:37:"lib/icons/die-netzmacher.de_200px.gif";s:4:"48b3";s:38:"lib/icons/made-in-nature.de_120x90.gif";s:4:"1bea";s:30:"lib/icons/typo3_quick_shop.gif";s:4:"2671";s:31:"lib/icons/your-logo_de-blue.gif";s:4:"19f7";s:31:"lib/icons/your-logo_de-grey.gif";s:4:"1fbc";s:36:"lib/icons/your-logo_default-blue.gif";s:4:"710c";s:36:"lib/icons/your-logo_default-grey.gif";s:4:"6fdc";s:44:"lib/userfunc/class.tx_quickshop_userfunc.php";s:4:"12fb";s:26:"lib/userfunc/locallang.xml";s:4:"09d5";s:38:"res/images/alternate_image_400x300.gif";s:4:"91b6";s:44:"res/images/quick_shop_header_image_210px.jpg";s:4:"6cd7";s:34:"res/pdf/typo3-quick-shop-draft.pdf";s:4:"f9bc";s:36:"res/samples/realurl/realurl_conf.php";s:4:"ef1e";s:25:"res/templates/default.css";s:4:"c939";s:26:"res/templates/default.tmpl";s:4:"0261";s:20:"static/constants.txt";s:4:"8c63";s:16:"static/setup.txt";s:4:"e68e";s:26:"static/caddy/constants.txt";s:4:"dac2";}',
+	'suggests' => array(
+		'quickshop_installer' => '',
+	),
+);
+
+?>
---
+++ branches/lib/userfunc/class.tx_quickshop_userfunc.php
@@ -0,0 +1,323 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2013 - Dirk Wildt <http://wildt.at.die-netzmacher.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+* Class provides methods for the extension manager.
+*
+* @author    Dirk Wildt <http://wildt.at.die-netzmacher.de>
+* @package    TYPO3
+* @subpackage    quick_shop
+* @version  0.0.2
+* @since    0.0.1
+*/
+
+
+  /**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ *
+ *
+ *   49: class tx_quickshop_userfunc
+ *   67:     function promptCheckUpdate()
+ *  102:     function promptCurrIP()
+ *
+ * TOTAL FUNCTIONS: 2
+ * (This index is automatically created/updated by the extension "extdeveval")
+ *
+ */
+class tx_quickshop_userfunc
+{
+  
+ /**
+  * Extension key
+  *
+  * @var string
+  */
+  private $extKey = 'quick_shop';
+
+ /**
+  * Extension configuration
+  *
+  * @var array
+  */
+  private $arr_extConf = null;
+  
+ /**
+  * Max width of div tags
+  *
+  * @var string
+  */
+  private $maxWidth = "600px";
+  
+ /**
+  * Version of TYPO3 (sample: 4.7.7 -> 4007007)
+  *
+  * @var string
+  */
+  public $typo3Version = null;
+
+
+
+/**
+ * Constructor. The method initiate the parent object
+ *
+ * @param    object        The parent object
+ * @return    void
+ */
+  function __construct( $pObj )
+  {
+    $this->pObj = $pObj;
+  }
+
+
+
+//  /**
+//   * pageWizard( ): Builds an input form that also includes the link popup wizard.
+//   * @param        array        Parameter array.  Contains fieldName and fieldValue.
+//   * @return        string        HTML output for form widget.
+//   * @version 0.0.1
+//   * @since   0.0.1
+//   */
+//  public function pageWizard( $params )
+//  {
+//    /* Pull the current fieldname and value from constants */
+//    $fieldName  = $params['fieldName'];
+//    $fieldValue = $params['fieldValue'];
+//    
+//    $input = '<input style="margin-right: 3px;" name="'. $fieldName .'" value="'. $fieldValue .'" />';
+//
+//    /* @todo     Don't hardcode the inclusion of the wizard this way.  Use more backend APIs. */
+//    $wizard = '<a href="#" onclick="this.blur(); vHWin=window.open(\'../../../../typo3/browse_links.php?mode=wizard&amp;P[field]='. $fieldName .'&amp;P[formName]=editForm&amp;P[itemName]='. $fieldName .'&amp;P[fieldChangeFunc][typo3form.fieldGet]=null&amp;P[fieldChangeFunc][TBE_EDITOR_fieldChanged]=null\',\'popUpID478be36b64\',\'height=300,width=500,status=0,menubar=0,scrollbars=1\'); vHWin.focus(); return false;"><img src="../../../../typo3/sysext/t3skin/icons/gfx/link_popup.gif" width="16" height="15" border="0" alt="Link" title="Link" /></a>';
+//
+//    return $input.$wizard;
+//  }
+
+  
+  
+  /**
+   * promptEvaluatorTYPO3version(): Displays the quick start message.
+   *
+   * @return  string    message wrapped in HTML
+   * @version 0.0.1
+   * @since   0.0.1
+   */
+  public function promptEvaluatorTYPO3version()
+  {
+//.message-notice
+//.message-information
+//.message-ok
+//.message-warning
+//.message-error
+
+    $prompt = null;
+
+    $this->set_TYPO3Version( );
+    
+    switch( true )
+    {
+      case( $this->typo3Version < 4005000 ):
+          // Smaller than 4.5
+        $prompt = $prompt . '
+          <div class="typo3-message message-warning" style="max-width:' . $this->maxWidth . ';">
+            <div class="message-body">
+              ' . $GLOBALS['LANG']->sL('LLL:EXT:quick_shop/lib/userfunc/locallang.xml:promptEvaluatorTYPO3version45smaller'). '
+            </div>
+          </div>
+          ';
+//        $prompt = $prompt . '
+//          <div class="typo3-message message-information" style="max-width:' . $this->maxWidth . ';">
+//            <div class="message-body">
+//              ' . $GLOBALS['LANG']->sL('LLL:EXT:quick_shop/lib/userfunc/locallang.xml:promptEvaluatorIncludeCss4-6'). '
+//            </div>
+//          </div>
+//          ';
+        break;
+      case( $this->typo3Version < 4006000 ):
+          // Greater than 4.7
+        $prompt = $prompt . '
+          <div class="typo3-message message-ok" style="max-width:' . $this->maxWidth . ';">
+            <div class="message-body">
+              ' . $GLOBALS['LANG']->sL('LLL:EXT:quick_shop/lib/userfunc/locallang.xml:promptEvaluatorTYPO3version46smaller'). '
+            </div>
+          </div>
+          ';
+//        $prompt = $prompt . '
+//          <div class="typo3-message message-information" style="max-width:' . $this->maxWidth . ';">
+//            <div class="message-body">
+//              ' . $GLOBALS['LANG']->sL('LLL:EXT:quick_shop/lib/userfunc/locallang.xml:promptEvaluatorIncludeCss4-6'). '
+//            </div>
+//          </div>
+//          ';
+        break;
+      case( $this->typo3Version < 4007000 ):
+          // Greater than 4.7
+        $prompt = $prompt . '
+          <div class="typo3-message message-ok" style="max-width:' . $this->maxWidth . ';">
+            <div class="message-body">
+              ' . $GLOBALS['LANG']->sL('LLL:EXT:quick_shop/lib/userfunc/locallang.xml:promptEvaluatorTYPO3version47smaller'). '
+            </div>
+          </div>
+          ';
+//        $prompt = $prompt . '
+//          <div class="typo3-message message-information" style="max-width:' . $this->maxWidth . ';">
+//            <div class="message-body">
+//              ' . $GLOBALS['LANG']->sL('LLL:EXT:quick_shop/lib/userfunc/locallang.xml:promptEvaluatorIncludeCss4-6'). '
+//            </div>
+//          </div>
+//          ';
+        break;
+      case( $this->typo3Version < 4008000 ):
+          // Greater than 4.7
+        $prompt = $prompt . '
+          <div class="typo3-message message-ok" style="max-width:' . $this->maxWidth . ';">
+            <div class="message-body">
+              ' . $GLOBALS['LANG']->sL('LLL:EXT:quick_shop/lib/userfunc/locallang.xml:promptEvaluatorTYPO3version48smaller'). '
+            </div>
+          </div>
+          ';
+        break;
+//      case( ( $this->typo3Version >= 4006000 ) && ( $this->typo3Version < 4007000 ) ):
+      default:
+          // Equal to 4.6
+        $prompt = $prompt . '
+          <div class="typo3-message message-ok" style="max-width:' . $this->maxWidth . ';">
+            <div class="message-body">
+              ' . $GLOBALS['LANG']->sL('LLL:EXT:quick_shop/lib/userfunc/locallang.xml:promptEvaluatorTYPO3version48orGreater'). '
+            </div>
+          ';
+        break;
+    }
+        
+    return $prompt;
+  }
+
+  
+  
+  /**
+   * promptExternalLinks(): Displays the quick start message.
+   *
+   * @return  string    message wrapped in HTML
+   * @version 0.0.1
+   * @since   0.0.1
+   */
+  public function promptExternalLinks()
+  {
+//.message-notice
+//.message-information
+//.message-ok
+//.message-warning
+//.message-error
+
+      $prompt = null;
+
+      $prompt = $prompt . '
+<div class="message-body" style="max-width:600px;">
+  ' . $GLOBALS['LANG']->sL('LLL:EXT:quick_shop/lib/userfunc/locallang.xml:promptExternalLinksBody') . '
+</div>';
+
+    return $prompt;
+  }
+
+  
+  
+/**
+ * set_TYPO3Version( ):
+ *
+ * @return  void
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+  private function set_TYPO3Version( )
+  {
+      // RETURN : typo3Version is set
+    if( $this->typo3Version !== null )
+    {
+      return;
+    }
+      // RETURN : typo3Version is set
+    
+      // Set TYPO3 version as integer (sample: 4.7.7 -> 4007007)
+    list( $main, $sub, $bugfix ) = explode( '.', TYPO3_version );
+    $version = ( ( int ) $main ) * 1000000;
+    $version = $version + ( ( int ) $sub ) * 1000;
+    $version = $version + ( ( int ) $bugfix ) * 1;
+    $this->typo3Version = $version;
+      // Set TYPO3 version as integer (sample: 4.7.7 -> 4007007)
+
+    if( $this->typo3Version < 3000000 )
+    {
+      $prompt = '<h1>ERROR</h1>
+        <h2>Unproper TYPO3 version</h2>
+        <ul>
+          <li>
+            TYPO3 version is smaller than 3.0.0
+          </li>
+          <li>
+            constant TYPO3_version: ' . TYPO3_version . '
+          </li>
+          <li>
+            integer $this->typo3Version: ' . ( int ) $this->typo3Version . '
+          </li>
+        </ul>
+          ';
+      die ( $prompt );
+    }
+  }
+  /**
+   * promptSponsors( ): Displays the quick start message.
+   *
+   * @return  string    message wrapped in HTML
+   * @access  public
+   * @version 3.0.0
+   * @since   3.0.0
+   */
+  public function promptSponsors()
+  {
+//.message-notice
+//.message-information
+//.message-ok
+//.message-warning
+//.message-error
+
+      $prompt = null;
+
+      $prompt = $prompt . '
+<div class="message-body" style="max-width:600px;">
+  ' . $GLOBALS['LANG']->sL('LLL:EXT:quick_shop/lib/userfunc/locallang.xml:promptSponsors') . '
+</div>';
+
+    return $prompt;
+  }
+
+  
+  
+}
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/quick_shop/lib/class.tx_quickshop_userfunc.php'])
+{
+  include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/quick_shop/lib/class.tx_quickshop_userfunc.php']);
+}
+
+?>
