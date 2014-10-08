d5744835ca824797867b6a6cf0864b3fe802ee6f
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-10-08 21:36:04 +0000

    6.0.0 +++ Manual
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/qshop@86546 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_emconf.php
+++ trunk/ext_emconf.php
@@ -13,9 +13,9 @@
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Quick Shop  –  responsive, ready-to-use, one-click-installation',
 	'description' => ''
-  . 'http://typo3-quick-shop.de Quick Shop is optimised for desktops, tablets and smartphones. '
+  . 'http://typo3-quick-shop.de. Quick Shop is optimised for desktops, tablets and smartphones. '
   . 'You are welcome to install Quick Shop with one mouse-click – responsive and ready-to-use. '
-  . 'Quick Shop is e-commerce for small budget and small-scale enterprises.'
+  . 'Quick Shop is e-commerce for small budget and small-scale enterprises. '
   . 'Manual: http://typo3-quick-shop.de/typo3conf/ext/quick_shop/doc/manual.pdf',
 	'category' => 'plugin',
 	'shy' => 0,
