2aa7285a7f08fa9a6f655b62bfcb1710205787f9
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-03-02 14:21:29 +0000

    6.0.11 +++ #i0024
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/qshop/trunk@88453 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/62037/page/setup.ts
+++ Configuration/TypoScript/62037/page/setup.ts
@@ -5,7 +5,6 @@ page {
   }
 }
 
-//[globalVar = GP:tx_browser_pi1|itemUid > 0] && [globalVar = TSFE:id = {$plugin.quick_shop.pages.shop}]
 [globalVar = GP:tx_browser_pi1|{$plugin.tx_browser_pi1.templates.listview.url.0.showUid} > 0] && [globalVar = TSFE:id = {$plugin.quick_shop.pages.shop}]
   page.10 {
     subparts {
@@ -19,18 +18,6 @@ page {
 [end]
 
 [globalVar = GP:tx_browser_pi1|pointer > 0] && [globalVar = TSFE:id = {$plugin.quick_shop.pages.shop}]
-  page.10 {
-    subparts {
-      columnscontent {
-        select {
-          where = colPos=0 AND list_type = "browser_pi1"
-        }
-      }
-    }
-  }
-[end]
-
-[globalVar = GP:sword] && [globalVar = TSFE:id = {$plugin.quick_shop.pages.shop}]
   page.10 {
     subparts {
       columnscontent {
