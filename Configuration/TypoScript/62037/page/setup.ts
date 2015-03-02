7ff0c52f3142d56e38298d73786d1c39b48c2744
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-03-02 14:03:56 +0000

    6.0.11 +++ #i0024
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/qshop/trunk@88452 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/62037/page/setup.ts
+++ Configuration/TypoScript/62037/page/setup.ts
@@ -5,7 +5,32 @@ page {
   }
 }
 
-[globalVar = GP:tx_browser_pi1|itemUid > 0] && [globalVar = TSFE:id = {$plugin.quick_shop.pages.shop}]
+//[globalVar = GP:tx_browser_pi1|itemUid > 0] && [globalVar = TSFE:id = {$plugin.quick_shop.pages.shop}]
+[globalVar = GP:tx_browser_pi1|{$plugin.tx_browser_pi1.templates.listview.url.0.showUid} > 0] && [globalVar = TSFE:id = {$plugin.quick_shop.pages.shop}]
+  page.10 {
+    subparts {
+      columnscontent {
+        select {
+          where = colPos=0 AND list_type = "browser_pi1"
+        }
+      }
+    }
+  }
+[end]
+
+[globalVar = GP:tx_browser_pi1|pointer > 0] && [globalVar = TSFE:id = {$plugin.quick_shop.pages.shop}]
+  page.10 {
+    subparts {
+      columnscontent {
+        select {
+          where = colPos=0 AND list_type = "browser_pi1"
+        }
+      }
+    }
+  }
+[end]
+
+[globalVar = GP:sword] && [globalVar = TSFE:id = {$plugin.quick_shop.pages.shop}]
   page.10 {
     subparts {
       columnscontent {
