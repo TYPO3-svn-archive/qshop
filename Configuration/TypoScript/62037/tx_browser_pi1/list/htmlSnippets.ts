b77a0cf604bc5375bec6beeae45286a14576c614
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-04-16 12:01:40 +0000

    6.0.13 +++ #i0028
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/qshop/trunk@88810 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/62037/tx_browser_pi1/list/htmlSnippets.ts
+++ Configuration/TypoScript/62037/tx_browser_pi1/list/htmlSnippets.ts
@@ -17,20 +17,18 @@ plugin.tx_browser_pi1 {
             }
           }
           subparts {
-              // For foundation main_02.html
-            //searchform < plugin.tx_browser_pi1.displayList.master_templates.subparts.listview.searchform.simpleMapOrderFilter
-              // For foundation main_01.html
-            //searchform < plugin.tx_browser_pi1.displayList.master_templates.subparts.listview.searchform.simple
             listview = TEXT
             listview {
               value (
-                <div id="c###TT_CONTENT.UID###-listview-###MODE###" class="columns listview listview-content listview-###MODE### listview-content-###MODE###">
-                  <div class="row">
-                    <!-- ###LISTBODY### begin --><!-- ###LISTBODYITEM### begin -->
-                    ###TX_QUICKSHOP_PRODUCTS.TITLE###
-                    <!-- ###LISTBODYITEM### end --><!-- ###LISTBODY### end -->
-                  </div> <!-- /row -->
-                </div> <!-- /listview -->
+                <div class="row">
+                  <div id="c###TT_CONTENT.UID###-listview-###MODE###" class="columns listview listview-content listview-###MODE### listview-content-###MODE###">
+                    <div class="row">
+                      <!-- ###LISTBODY### begin --><!-- ###LISTBODYITEM### begin -->
+                      ###TX_QUICKSHOP_PRODUCTS.TITLE###
+                      <!-- ###LISTBODYITEM### end --><!-- ###LISTBODY### end -->
+                    </div> <!-- /row -->
+                  </div> <!-- /listview -->
+                </div><!-- /row -->
 )
             }
           }
