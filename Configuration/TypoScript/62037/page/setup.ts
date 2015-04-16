f69f4996ad9cee39479f59cec1dff25e79ba7493
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-04-16 14:11:29 +0000

    6.0.13 +++ #i0029
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/qshop/trunk@88815 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/62037/page/setup.ts
+++ Configuration/TypoScript/62037/page/setup.ts
@@ -1,16 +1,32 @@
-  'paket' => array(
-    array(
-      'GETvar'      => 'tx_browser_pi1[itemUid]' ,
-      'lookUpTable' => array(
-        'table'               => 'tx_quickshop_products',
-        'id_field'            => 'uid',
-        'alias_field'         => 'title',
-        'addWhereClause'      => ' AND NOT deleted AND NOT hidden',
-        'useUniqueCache'      => 1,
-        'useUniqueCache_conf' => array(
-          'strtolower'     => 1,
-          'spaceCharacter' => '-',
-        ),
-      ),
-    ),
-  ),
+page {
+  includeCSS {
+    // add the quick shop default css
+    quick_shop = EXT:quick_shop/Ressources/Public/Templates/default.css
+  }
+}
+
+// #i0021/#163
+[globalVar = GP:tx_browser_pi1|{$plugin.tx_browser_pi1.templates.listview.url.0.showUid} > 0] && [globalVar = TSFE:id = {$plugin.quick_shop.pages.shop}]
+  page.10 {
+    subparts {
+      columnscontent {
+        select {
+          where = colPos=0 AND list_type = "browser_pi1"
+        }
+      }
+    }
+  }
+[end]
+
+// #i0025
+[globalVar = GP:tx_browser_pi1|pointer > 0] && [globalVar = TSFE:id = {$plugin.quick_shop.pages.shop}]
+  page.10 {
+    subparts {
+      columnscontent {
+        select {
+          where = colPos=0 AND list_type = "browser_pi1"
+        }
+      }
+    }
+  }
+[end]
