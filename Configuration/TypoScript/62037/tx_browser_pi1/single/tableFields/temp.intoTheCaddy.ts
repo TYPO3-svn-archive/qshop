3513d55c0412d8636d02a4a09ef2d5026da8c3aa
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-11-20 23:12:26 +0000

    6.2.0 +++ Improve TypoScript
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/qshop/trunk@90259 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/62037/tx_browser_pi1/single/tableFields/temp.intoTheCaddy.ts
+++ Configuration/TypoScript/62037/tx_browser_pi1/single/tableFields/temp.intoTheCaddy.ts
@@ -1,83 +1,6 @@
   // Buy now
 temp.intoTheCaddy = COA
 temp.intoTheCaddy {
-  inTextRight = COA
-  inTextRight {
-      // if [in text right]
-    if =
-    if {
-      value = {$plugin.quick_shop.layout.caddy.position}
-      equals = in text right
-    }
-    wrap = <div style="float:right;padding:0 0 1em 1em;width:40%;"><ul class="pricing-table">|</ul></div>
-      // price
-    10 = TEXT
-    10 {
-      field = tx_quickshop_products.price
-      stdWrap {
-        numberFormat {
-          decimals = 2
-          dec_point {
-            cObject = TEXT
-            cObject {
-              value = .
-              stdWrap {
-                lang {
-                  de = ,
-                  en = .
-                }
-              }
-            }
-          }
-        }
-        noTrimWrap = || &euro;|
-      }
-      wrap  = <li class="price" style="font-weight:bold">|</li>
-    }
-      // tax, my_shipping_note
-    30 = COA
-    30 {
-        // tax
-      10 < plugin.tx_quickshop.templates.tax
-      10 {
-        1 {
-          wrap  = <li class="bullet-item">|</li>
-          wrap  = |<br />
-        }
-        2 {
-          wrap  = <li class="bullet-item">|</li>
-          wrap  = |<br />
-        }
-      }
-        // my_shipping_note
-      20 = TEXT
-      20 {
-        if {
-          isTrue = {$plugin.quick_shop.layout.caddy.shippingnote}
-        }
-        value = shipping
-        lang {
-          de = Versand
-          en = shipping
-        }
-        typolink.parameter = {$plugin.quick_shop.pages.shipping}
-        prepend = TEXT
-        prepend {
-          value = plus
-          lang {
-            de = zzgl.
-            en = plus
-          }
-        }
-        noTrimWrap = | ||
-      }
-      wrap  = <li class="bullet-item">|</li>
-    }
-    60 < plugin.tx_quickshop.templates.formWiSelect
-    60 {
-      wrap = <li class="caddyWoMargin">|</li>
-    }
-  }
   belowContent = COA
   belowContent {
       // if [at the bottom]
@@ -184,6 +107,83 @@ temp.intoTheCaddy {
     60 < plugin.tx_quickshop.templates.formWiSelect
     60 {
       wrap = <li class="columns small-12 medium-4 caddyWoMargin">|</li>
+    }
+  }
+  inTextRight = COA
+  inTextRight {
+      // if [in text right]
+    if =
+    if {
+      value = {$plugin.quick_shop.layout.caddy.position}
+      equals = in text right
+    }
+    wrap = <div style="float:right;padding:0 0 1em 1em;width:40%;"><ul class="pricing-table">|</ul></div>
+      // price
+    10 = TEXT
+    10 {
+      field = tx_quickshop_products.price
+      stdWrap {
+        numberFormat {
+          decimals = 2
+          dec_point {
+            cObject = TEXT
+            cObject {
+              value = .
+              stdWrap {
+                lang {
+                  de = ,
+                  en = .
+                }
+              }
+            }
+          }
+        }
+        noTrimWrap = || &euro;|
+      }
+      wrap  = <li class="price" style="font-weight:bold">|</li>
+    }
+      // tax, my_shipping_note
+    30 = COA
+    30 {
+        // tax
+      10 < plugin.tx_quickshop.templates.tax
+      10 {
+        1 {
+          wrap  = <li class="bullet-item">|</li>
+          wrap  = |<br />
+        }
+        2 {
+          wrap  = <li class="bullet-item">|</li>
+          wrap  = |<br />
+        }
+      }
+        // my_shipping_note
+      20 = TEXT
+      20 {
+        if {
+          isTrue = {$plugin.quick_shop.layout.caddy.shippingnote}
+        }
+        value = shipping
+        lang {
+          de = Versand
+          en = shipping
+        }
+        typolink.parameter = {$plugin.quick_shop.pages.shipping}
+        prepend = TEXT
+        prepend {
+          value = plus
+          lang {
+            de = zzgl.
+            en = plus
+          }
+        }
+        noTrimWrap = | ||
+      }
+      wrap  = <li class="bullet-item">|</li>
+    }
+    60 < plugin.tx_quickshop.templates.formWiSelect
+    60 {
+      wrap = <li class="caddyWoMargin">|</li>
     }
   }
 }
